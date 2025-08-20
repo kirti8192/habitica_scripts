8421e00b21186258ec2d62604762e820b3118b84
Keerthi Suria Kumar Arumugam <kirti8192@gmail.com>
2025-08-20 11:38:04 -0700

    add autocast

--- auto_cast_valorous_presence.py
+++ auto_cast_valorous_presence.py
@@ -1,9 +1,10 @@
 import requests
 
+# Habitica credentials
 user_id = "42bf3777-022a-4b8d-abdf-176f7b8e7be9"
 api_token = "b2d8430a-3c2d-45ba-9a40-af7372e00380"
 
-url = "https://habitica.com/api/v3/groups/party/quests/accept"
+url = "https://habitica.com/api/v3/user/class/cast/valorousPresence"
 
 headers = {
     "x-api-user": user_id,
@@ -14,4 +15,4 @@ headers = {
 
 r = requests.post(url, headers=headers)
 
-print("Quest Accept Response:", r.status_code, r.json())
+print("Cast Valorous Presence:", r.status_code, r.json())
